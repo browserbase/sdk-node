56cc2af53ccb34125c1175280b966d0f6047d515
Anirudh Kamath <github@kamath.io>
2024-10-27 23:46:51 -0700

    lint

--- examples/packages/extensions/browserbase-test/scripts/content.js
+++ examples/packages/extensions/browserbase-test/scripts/content.js
@@ -1,11 +1,12 @@
+/* eslint-disable prettier/prettier */
 const imageUrl = chrome.runtime.getURL("images/logo.png");
 window
-  .fetch(imageUrl)
-  .then((response) => {
-    if (response.ok) {
-      console.log("browserbase test extension image loaded");
-    }
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+	.fetch(imageUrl)
+	.then((response) => {
+		if (response.ok) {
+			console.log("browserbase test extension image loaded");
+		}
+	})
+	.catch((error) => {
+		console.log(error);
+	});
